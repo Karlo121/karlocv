1e97aff9e1216598fe405b14133e3e06363c7d8c
Karlo121 <51051819+Karlo121@users.noreply.github.com>
2020-07-23 19:26:09 +0200

    Updates

---
+++ precache-manifest.d8d666af12e53b5ed715b445acbea6ac.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7c8307bb4c1ef0114060b844cecf9045",
+    "url": "/karlocv%20/index.html"
+  },
+  {
+    "revision": "87ecb1e7e80910b9c9e5",
+    "url": "/karlocv%20/static/css/main.505541b4.chunk.css"
+  },
+  {
+    "revision": "726ddf069781bb4de07b",
+    "url": "/karlocv%20/static/js/2.e6ab62a8.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/karlocv%20/static/js/2.e6ab62a8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "87ecb1e7e80910b9c9e5",
+    "url": "/karlocv%20/static/js/main.dc8e4a8e.chunk.js"
+  },
+  {
+    "revision": "0413789ddb559c7edac6",
+    "url": "/karlocv%20/static/js/runtime-main.f6135d9e.js"
+  },
+  {
+    "revision": "df7e79f2cab9d0ccdb62b1b1e9247f61",
+    "url": "/karlocv%20/static/media/c.df7e79f2.png"
+  },
+  {
+    "revision": "ff60ab88cbff4016fd9f0629030b1fec",
+    "url": "/karlocv%20/static/media/csharp.ff60ab88.svg"
+  },
+  {
+    "revision": "8f047bb893c3c4ee94de0832935d7c89",
+    "url": "/karlocv%20/static/media/css.8f047bb8.png"
+  },
+  {
+    "revision": "f2282f94c041c37ef4db028e428e41c1",
+    "url": "/karlocv%20/static/media/html.f2282f94.png"
+  },
+  {
+    "revision": "afb47678875386aa6e81b65aa0640563",
+    "url": "/karlocv%20/static/media/js.afb47678.png"
+  },
+  {
+    "revision": "94a629c920c0eec941766a418268d54d",
+    "url": "/karlocv%20/static/media/python.94a629c9.png"
+  },
+  {
+    "revision": "e9d83ce8d1ceb1dfa4cec88dac5ef6db",
+    "url": "/karlocv%20/static/media/themepong1.e9d83ce8.png"
+  },
+  {
+    "revision": "d0686e7e266be3dc760f8e6024c5886e",
+    "url": "/karlocv%20/static/media/themepong2.d0686e7e.png"
+  },
+  {
+    "revision": "62718e15256628c52666dab7a650b094",
+    "url": "/karlocv%20/static/media/themepong3.62718e15.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/karlocv/precache-manifest.cf2bcdc5967a35b67834e7e6b94cb4b7.js"
+  "/karlocv%20/precache-manifest.d8d666af12e53b5ed715b445acbea6ac.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/karlocv/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/karlocv%20/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
